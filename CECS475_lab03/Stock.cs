7863aa6da5bfbb3a462ea6b950e59c8e46fc7a02
bethore <askforgeorge@gmail.com>
2016-02-09 16:26:50 -0800

    initial setup

---
+++ CECS475_lab03/Stock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CECS475_lab03
+{
+   class Stock
+   {
+      public string Name { get; private set; }
+
+      public int InitialValue { get; private set; }
+
+      public int MaxChange { get; private set; }
+
+      public int Notification { get; private set; }
+
+      public Stock( string name, int value, int change, int notify )
+      {
+         Name = name;
+         InitialValue = value;
+         MaxChange = change;
+         Notification = notify;
+      }
+
+      public void ChangeStockValue()
+      {
+         // code
+      }
+
+      public void Activate()
+      {
+         // code
+      }
+   }
+}
---
+++ CECS475_lab03/StockBroker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CECS475_lab03
+{
+   class StockBroker
+   {
+      public string Name { get; private set; }
+
+      public List<Stock> StockList {get; set;}
+
+      public StockBroker( string name )
+      {
+         Name = name;
+         StockList = new List<Stock>();
+      } 
+
+      public void AddStock(Stock s)
+      {
+         StockList.Add(new Stock(s.Name, s.InitialValue, s.MaxChange, s.Notification) { });
+      }
+   }
+}
